32bfb2294184f2c5ae8307b6643b9e4c59b58857
shayan <shayanmuhammad5001@gmail.com>
2024-08-23 20:15:11 +0500

    changes in the model

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0], prompt])
